e9c4807ba564d049054f2818fcc29819863d1ddf
powcoder <powcoder@163.com>
2022-12-14 12:38:30 +0800

    csc485 a1

---
+++ README.md
@@ -0,0 +1,9 @@
+# csc485 a1
+# 加微信 powcoder
+
+# QQ 1823890830
+
+# Programming Help Add Wechat powcoder
+
+# Email: powcoder@163.com
+
---
+++ q2/gpu-train.sh
@@ -0,0 +1,11 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+#!/usr/bin/env bash
+
+srun -p csc485 --gres gpu -c 2 python3 train.py
